53e63c17cd75b82fb6a9b70b9fb0d3956aab4520
Jason Fitzpatrick <jasonfitz01@yahoo.com>
2026-05-02 23:30:24 -0700

    Created pinmaps for box controller and main controller

---
+++ firmware/lib/Common/PinMap_Box.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Fan
+#define PIN_FAN_TACH 18
+#define PIN_FAN_PWM 19
+
+// Heater
+#define PIN_HEATER 2
+
+// Mister
+#define PIN_MISTER 10
+
+// Temp / RTH Sensor
+#define PIN_SENSOR_SCL 6
+#define PIN_SENSOR_SDA 7
+
+// Pump
+#define PIN_PUMP 1
---
+++ firmware/lib/Common/PinMap_Main.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Relay
+#define PIN_RELAY_1 1
+#define PIN_RELAY_2 0
+#define PIN_RELAY_3 19
+#define PIN_RELAY_4 18
+#define PIN_RELAY_5 4
+#define PIN_RELAY_6 5
+#define PIN_RELAY_7 6
+#define PIN_RELAY_8 7
